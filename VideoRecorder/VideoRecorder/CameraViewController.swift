c62d60f6e1b8337952e5d4770088c6eeea9d9642
Thomas Sabino-Benowitz <sabinowitz@gmail.com>
2020-02-12 13:43:15 -0500

    add video input configuration, movie file output

--- VideoRecorder/VideoRecorder/CameraViewController.swift
+++ VideoRecorder/VideoRecorder/CameraViewController.swift
@@ -7,8 +7,12 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraViewController: UIViewController {
+    
+    lazy private var captureSession = AVCaptureSession()
+    lazy private var fileOutput = AVCaptureMovieFileOutput()
 
     @IBOutlet var recordButton: UIButton!
     @IBOutlet var cameraView: CameraPreviewView!
@@ -19,12 +23,100 @@ class CameraViewController: UIViewController {
 
 		// Resize camera preview to fill the entire screen
 		cameraView.videoPlayerView.videoGravity = .resizeAspectFill
+        
+        setUpCamera()
+        
 	}
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        captureSession.startRunning()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        captureSession.stopRunning()
+    }
 
 
+    private func setUpCamera(){
+        let camera = bestCamera()
+        
+        //connfiguration
+        
+        captureSession.beginConfiguration()
+
+        // Inputs
+        
+        guard let cameraInput = try? AVCaptureDeviceInput(device: camera) else {
+            fatalError("Device configured incorrectly")
+        }
+        
+        guard captureSession.canAddInput(cameraInput) else {
+            fatalError("unable to add camera input")
+        }
+        
+        captureSession.addInput(cameraInput)
+        
+        //1920x1080p
+        if captureSession.canSetSessionPreset(.hd1920x1080) {
+            captureSession.sessionPreset = .hd1920x1080
+        }
+        
+        // Microphone
+        
+        // Outputs
+        
+        guard captureSession.canAddOutput(fileOutput) else {
+            fatalError("Cannot add file output")
+            
+        }
+        
+        captureSession.addOutput(fileOutput)
+        
+        
+        // commit configuration
+        
+        captureSession.commitConfiguration()
+        
+        cameraView.session = captureSession
+    }
+    
+    private func bestCamera() -> AVCaptureDevice {
+        // front/back
+        //wide angle, ultra wide angle, depth, zoom lens
+        
+        // try ultra wide lens
+        if let device = AVCaptureDevice.default(.builtInUltraWideCamera, for: .video, position: .back) {
+            return device
+        }
+        
+        //try wide angle lens
+        if let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) {
+            return device
+        }
+        
+        fatalError("provide Ui, NO CAMERAS AVAILABLE ON THE DEVICE (or you're running a simulator)")
+        
+    }
+    
     @IBAction func recordButtonPressed(_ sender: Any) {
-
+        toggleRecord()
 	}
+   
+    var isRecording: Bool {
+        fileOutput.isRecording
+    }
+    
+    func toggleRecord() {
+        if isRecording {
+            fileOutput.stopRecording()
+        } else {
+            fileOutput.startRecording(to: newRecordingURL(), recordingDelegate: self)
+        }
+    }
 	
 	/// Creates a new file URL in the documents directory
 	private func newRecordingURL() -> URL {
@@ -37,5 +129,25 @@ class CameraViewController: UIViewController {
 		let fileURL = documentsDirectory.appendingPathComponent(name).appendingPathExtension("mov")
 		return fileURL
 	}
+    private func updateViews() {
+        recordButton.isSelected = isRecording
+    }
 }
 
+extension CameraViewController: AVCaptureFileOutputRecordingDelegate {
+   func fileOutput(_ output: AVCaptureFileOutput, didStartRecordingTo fileURL: URL, from connections: [AVCaptureConnection]) {
+         
+        updateViews()
+    
+     }
+    
+    func fileOutput(_ output: AVCaptureFileOutput, didFinishRecordingTo outputFileURL: URL, from connections: [AVCaptureConnection], error: Error?) {
+        if let error = error {
+            print("Error saving recording: \(error)")
+        }
+        print("URL: \(outputFileURL.path)")
+        updateViews()
+    }
+    
+  
+}
